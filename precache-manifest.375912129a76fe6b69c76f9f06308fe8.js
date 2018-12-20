aa6cce6cb03932bf56d6529054e542fa82cb6094
Marvin Gorecki <marvin.gorecki@nexenio.com>
2018-12-20 10:43:29 +0100

    Updates

---
+++ precache-manifest.375912129a76fe6b69c76f9f06308fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BAuth-Demo-Dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f3ceb5495270e109b21",
+    "url": "/BAuth-Demo-Dashboard/static/js/runtime~main.1f3ceb54.js"
+  },
+  {
+    "revision": "e8fdfdb9ff8f73993125",
+    "url": "/BAuth-Demo-Dashboard/static/js/main.e8fdfdb9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/BAuth-Demo-Dashboard/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e8fdfdb9ff8f73993125",
+    "url": "/BAuth-Demo-Dashboard/static/css/main.8be90014.chunk.css"
+  },
+  {
+    "revision": "88974ac7d9cd753a0f11ee8e24259158",
+    "url": "/BAuth-Demo-Dashboard/index.html"
+  }
+];
