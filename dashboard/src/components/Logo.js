a1460d2fdac2b667aab17daa822ebacb723edda1
k-johannes <k.johannes@protonmail.com>
2019-11-15 11:47:02 +0100

    update to an older version

--- dashboard/src/components/Logo.js
+++ dashboard/src/components/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
       </div>
       <div className="col s6 m6 l4 offset-s0 offset-m0 offset-l0 right-align">
         <img className="header-logo" alt="Logo" src="https://uploads-ssl.webflow.com/5d6fe8bf5db14443465e7157/5d6ffa32ea5148650e2bbb64_light%4032x-p-2600.png" />
-      </div>
+        </div>
     </div>
   );
 }
