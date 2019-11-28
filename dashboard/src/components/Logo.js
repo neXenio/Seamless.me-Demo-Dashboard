e2a5c8118078eadd12ba8e1f9215d5bd53514ea3
k-johannes <k.johannes@protonmail.com>
2019-11-28 18:48:31 +0100

    added links to asset folder

--- dashboard/src/components/Logo.js
+++ dashboard/src/components/Logo.js
@@ -4,10 +4,10 @@ const Logo = () => {
   return (
     <div className="row">
       <div className="col s6 m6 l4 offset-s0 offset-m0 offset-l2 left-align">
-        <img className="header-logo" alt="Logo" src="https://uploads-ssl.webflow.com/5d6fe8bf5db14443465e7157/5d6ff570be2eaddc51b71c28_White.svg" />
+        <img className="header-logo" alt="Logo" src={require("../assets/seamless.me-logo.svg")} />
       </div>
       <div className="col s6 m6 l4 offset-s0 offset-m0 offset-l0 right-align">
-        <img className="header-logo" alt="Logo" src="https://uploads-ssl.webflow.com/5d6fe8bf5db14443465e7157/5d6ffa32ea5148650e2bbb64_light%4032x-p-2600.png" />
+        <img className="header-logo" alt="Logo" src={require("../assets/nexenio-logo.png")} />
       </div>
     </div>
   );
