52d5cb4039ef82c956fc70435610df6f3ea81e6c
k-johannes <k.johannes@protonmail.com>
2019-12-04 12:30:46 +0100

    Comment out COMPARISON_CHART_PLOT_DURATION constant

--- dashboard/src/components/Visualization.js
+++ dashboard/src/components/Visualization.js
@@ -6,7 +6,7 @@ import { Col, Collapsible, CollapsibleItem, Icon, Button } from 'react-materiali
 
 // CHART
 const CHART_PLOT_DURATION = 30 * 1000;
-const COMPARISON_CHART_PLOT_DURATION = 10 * 1000;
+// const COMPARISON_CHART_PLOT_DURATION = 10 * 1000;
 const MINIMUM_DATA_AGE = 500;
 const RENDERING_INTERVAL = 50;
 let timestampOffset = 0;
