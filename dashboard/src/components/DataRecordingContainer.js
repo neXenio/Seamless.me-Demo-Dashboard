4e4b66c244ea7d33197f0e19ef2ae0bd90317be3
k-johannes <k.johannes@protonmail.com>
2019-12-05 11:22:43 +0100

    Add default data type id

--- dashboard/src/components/DataRecordingContainer.js
+++ dashboard/src/components/DataRecordingContainer.js
@@ -113,7 +113,6 @@ class DataRecordingContainer {
 
   getDataTimestampsForComparison(id, startTimestamp, endTimestamp) {
     let timestamps = [];
-    // let maximumAggregationTimestamp = Date.now() - MINIMUM_DATA_AGE - timestampOffset;
     this.getData(id)
       .filter(data => data.aggregationTimestamp > startTimestamp && data.aggregationTimestamp < endTimestamp)
       .forEach(data => timestamps.push(data.aggregationTimestamp));
