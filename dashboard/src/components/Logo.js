d5322badc4aafdee2cfdfe2f715f1f5030d53e6e
k-johannes <k.johannes@protonmail.com>
2019-12-11 12:27:26 +0100

    Import logos instead of require them in code

--- dashboard/src/components/Logo.js
+++ dashboard/src/components/Logo.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import seamlessLogo from '../assets/seamless.me-logo.svg';
+import nexenioLogo from '../assets/nexenio-logo.png';
 
 const Logo = () => {
   return (
     <div className="row">
       <div className="col s6 m6 l4 offset-s0 offset-m0 offset-l2 left-align">
-        <img className="header-logo" alt="Logo" src={require("../assets/seamless.me-logo.svg")} />
+        <img className="header-logo" alt="Logo" src={seamlessLogo} />
       </div>
       <div className="col s6 m6 l4 offset-s0 offset-m0 offset-l0 right-align">
-        <img className="header-logo" alt="Logo" src={require("../assets/nexenio-logo.png")} />
+        <img className="header-logo" alt="Logo" src={nexenioLogo} />
       </div>
     </div>
   );
